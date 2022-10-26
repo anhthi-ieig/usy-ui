c0d106333aeda1e9993c2391b51548e889956e5f
Thi Nguyen <anhthi.ieig@gmail.com>
2022-10-26 07:51:40 +0700

    Add ESLint and Prettier

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,18 +1,15 @@
-import React from "react";
-import { ComponentMeta, ComponentStory } from "@storybook/react";
-import Button from "./Button";
-import { IButtonProps } from "./Button.interfaces";
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import Button from './Button';
+import { IButtonProps } from './Button.interfaces';
 
 export default {
-  title: "Usy UI/Button",
+  title: 'Usy UI/Button',
   component: Button,
 } as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = (args: IButtonProps) => (
-  <Button {...args} />
-);
+const Template: ComponentStory<typeof Button> = (args: IButtonProps) => <Button {...args} />;
 
 export const ClickMe = Template.bind({});
 ClickMe.args = {
-  label: "Hello World",
+  label: 'Hello World',
 };
